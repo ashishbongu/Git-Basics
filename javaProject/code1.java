1b5a127da37df3cae77dc5157d946e73c4834cbd
ashish bongu <bonguashish532222@gmail.com>
2026-02-08 00:19:31 +0530

    adding code1

---
+++ javaProject/code1.java
@@ -0,0 +1,14 @@
+public class code1 {
+
+    private int x;
+    private int y;
+
+    public code1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int add() {
+        return x + y;
+    }
+}
