447585cf6dc07b2c9681cb2a83aa80923129d33c
ashish bongu <bonguashish532222@gmail.com>
2026-02-08 00:23:59 +0530

    added new object

---
+++ javaProject/code2.java
@@ -0,0 +1,7 @@
+public class Code2{
+    public static void main(String[] args) {
+        code1 obj = new code1(5, 10);
+        int result = obj.add();
+        System.out.println("The sum is: " + result);
+    }
+}
